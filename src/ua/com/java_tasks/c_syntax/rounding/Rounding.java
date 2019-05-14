7b30666c63c86b70471071fc7088621e5b32d4b2
Aliiev Rustam <rustam.miko@gmail.com>
2019-05-15 00:51:08 +0300

    c_syntactic_constructions
    rounding

--- src/ua/com/java_tasks/c_syntax/rounding/Rounding.java
+++ src/ua/com/java_tasks/c_syntax/rounding/Rounding.java
@@ -2,30 +2,42 @@ package ua.com.java_tasks.c_syntax.rounding;
 
 import java.util.Scanner;
 
+/**
+ * Написать программу, которая считывает значения с плавающей точкой и печатает округленные значения.
+ * Не использовать стандартную функцию округления.
+ * Использовать приведение типов.
+ */
 public class Rounding {
+
     public static void main(String[] args) {
         System.out.println("Enter a fractional number");
         System.out.println("I've round it: " + roundIt(inputting()));
     }
 
-    private static double inputting(){
+    private static double inputting() {
         Scanner scanner = new Scanner(System.in);
         double inputNumber = scanner.nextDouble();
         scanner.close();
         return inputNumber;
     }
 
-    static double roundIt(double input){
-        if (isRoundUp(input)){
-            input = (int)input + 1;
-        }
-        else {
+    /**
+     * Round it double.
+     * Accepts e real number and returns rounded one.
+     *
+     * @param input the input
+     * @return the double
+     */
+    static double roundIt(double input) {
+        if (isRoundUp(input)) {
+            input = (int) input + 1;
+        } else {
             input = (int) input;
         }
         return input;
     }
 
-    private static boolean isRoundUp(double input){
-        return input%1 > 0.4;
+    private static boolean isRoundUp(final double input) {
+        return input % 1 > 0.4;
     }
 }
