0426825ecba364f552d7187ea2a1889d1b0d64eb
Aliiev Rustam <rustam.miko@gmail.com>
2019-06-10 22:37:22 +0300

    c_syntactic_constructions
    Task12BinaryNotation

--- src/ua/com/topic03_syntax/Task12BinaryNotation.java
+++ src/ua/com/topic03_syntax/Task12BinaryNotation.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 /**
  * Created by Rustam on 09.06.2019
- *
+ * <p>
  * Изменить программу двоичного представления числа так,
  * чтобы не выводились нули вначале. Использовать булеву
  * переменную для индикации того, первый ли это ноль.
  */
 public class Task12BinaryNotation {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int b = scanner.nextInt();
         convertToBinaryForm(b);
     }
 
+    /**
+     * Convert to binary form string.
+     *
+     * @param b the b
+     * @return the string
+     */
     static String convertToBinaryForm(int b) {
         String result ="";
         boolean flag = true;
