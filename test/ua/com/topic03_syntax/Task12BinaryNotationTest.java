ea53d0029ddb14663b495634b571fbdc1714d759
Aliiev Rustam <rustam.miko@gmail.com>
2019-06-10 22:39:45 +0300

    c_syntactic_constructions
    Task12BinaryNotation

--- test/ua/com/topic03_syntax/Task12BinaryNotationTest.java
+++ test/ua/com/topic03_syntax/Task12BinaryNotationTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 
 
 class Task12BinaryNotationTest {
-    @Test
+    protected @Test
     void SHOULD_RETURN_BINARY_NUMBER_WITHOUT_ZEROS(){
         Assert.assertEquals(Task12BinaryNotation.convertToBinaryForm(789), Integer.toBinaryString(789));
         Assert.assertEquals(Task12BinaryNotation.convertToBinaryForm(-98), Integer.toBinaryString(-98));
